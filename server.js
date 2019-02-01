b31e94a3a616adf89bb396479ceac53d58629db4
Saurabh Nigam <sanigam@paypal.com>
2019-02-01 16:25:52 +0530

    testing

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 var http = require('http');
 var path = require('path');
 var braintree = require('braintree');
-var gateway = braintree.connect({environment: braintree.Environment.Sandbox,
-	merchantId: "bhbf9cdcrkx3ffj7",
-	publicKey: "w3bc5qymq3z249cg",
-	privateKey: "36dad87c9d0e6f383bddc5fe5a06f184"
+var gateway = braintree.connect({environment: braintree.Environment.Production,
+	//merchantId: "bhbf9cdcrkx3ffj7",
+	//publicKey: "w3bc5qymq3z249cg",
+	//privateKey: "36dad87c9d0e6f383bddc5fe5a06f184"
 	//accessToken: 'access_token$sandbox$twqz54969tjcvnzb$135fdf7d7c6d230b586fe0a8dcf647a4'
 	//accessToken: 'access_token$sandbox$cmsjrxqjrjzbcz2r$3ea9b37593fb87eccaa70d92ddf6babf'
-	//accessToken: 'access_token$production$t2kz2xvnj6qz54cr$c74d08d4cd2a22d24146cdfc62f5489f'
+	accessToken: 'access_token$production$t2kz2xvnj6qz54cr$c74d08d4cd2a22d24146cdfc62f5489f'
 });
 
 var async = require('async');
@@ -328,7 +328,7 @@ router.post("/checkout", function (req, res) {
 	console.log(nonce);
 	var payLoad = buildbtPaymentRequestPayload(req.body);
 	payLoad.paymentMethodNonce = nonce;
-	//payLoad.options.storeInVaultOnSuccess = true;
+	payLoad.options.storeInVaultOnSuccess = true;
 	//payLoad.deviceData = req.body.deviceData;
 	console.log(payLoad);
 
