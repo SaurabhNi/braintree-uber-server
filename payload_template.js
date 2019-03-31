0b3a02b2d0430f885691c00df5c27857befbc83c
Saurabh Nigam <sanigam@paypal.com>
2019-03-31 11:40:27 +0530

    changed BT gateway credentials to CIB account

--- payload_template.js
+++ payload_template.js
@@ -64,7 +64,7 @@ return {
 			submitForSettlement: false,
 		"paypal":{
 		"description":"Your Uber ride fare",
-		"payee_email":"sanigam-facilitator@paypal.com"
+		"payee_email":"testbaflows@paypal.com"
 		}
 }
 }
