730960de06c4d0ef7f9c90268b9fbf5c9c75a2c4
Saurabh Nigam <sanigam@paypal.com>
2019-04-01 20:38:38 +0530

    changing to Suzie's account

--- payload_template.js
+++ payload_template.js
@@ -64,7 +64,7 @@ return {
 			submitForSettlement: false,
 		"paypal":{
 		"description":"Your Uber ride fare",
-		"payee_email":"sanigam-paypal@paypal.com"
+		"payee_email":"sanigam-facilitator@paypal.com"
 		}
 }
 }
